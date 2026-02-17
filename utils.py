a545334de360341c19aab33f604c9b2381654bc8
tibertun <tominasonyaa@gmail.com>
2026-02-17 12:03:26 +0200

    Add function in utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
 
 
 def isPrime(n):

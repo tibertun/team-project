0099a68c83c01b093d3d8ad6738cb6bcfc66805f
Alicedomik <alicedomnik6@gmail.com>
2026-02-17 12:05:17 +0200

    changed main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from utils import factorial, gcd, is_prime, is_power_of_five
+from utils import factorial, gcd, isPrime, is_power_of_five
 
 print("Factorial of 5:", factorial(5))
 print("GCD of 18 and 24:", gcd(18, 24))
 print("Is 7 prime?", is_prime(7))
 print("Is 125 power of five?", is_power_of_five(125))
+print(isPrime(125))
 

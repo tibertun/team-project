dbec078ee31f3bcf4cee2b6b3c74ef92a600f19c
saharomanenko2008-coder <saharomanenko2008@gmail.com>
2026-02-17 12:08:21 +0200

    print function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from utils import factorial, isPrime
+from utils import factorial, isPrime , is_power_of_five
 
 
 print("Factorial of 5:", factorial(5))
 print(isPrime(5))
-
+print(is_power_of_five(5))

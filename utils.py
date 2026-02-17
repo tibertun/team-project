6a3d9725d2a708f1a5dc95a1c62157e372adaf41
Alicedomik <alicedomnik6@gmail.com>
2026-02-17 11:30:35 +0200

    Added new function

--- utils.py
+++ utils.py
@@ -4,3 +4,11 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
